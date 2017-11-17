135a7243c2de3609d91a0d4af8fd49d589b71e72
Austin Berger <austin.berger2@gmail.com>
2017-11-17 14:57:00 -0600

    Updated Segmentation
    Slic can now recenter and run multiple times.  Also works for non-square images now.

--- SlicLibraryImplementation.py
+++ SlicLibraryImplementation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import random
+import segmentation
 from skimage.segmentation import slic
 
 ##Return a numpy array close to a circle for use as a kernel
@@ -9,14 +10,16 @@ def circle_n(radius):
     cv2.circle(img, (radius, radius), radius, 1, -1)
     return img
 
+plus = np.array([[0,1,0],[1,1,1],[0,1,0]])
+
 ## SLIC Parameters
 NUM_SEGMENTS = 150 #100
-SIGMA = 4
+SIGMA = 4#4
 COMPACTNESS = 80 #50 #M parameter
 ENFORCE_CONNECT = 1
 
 ##Import image
-img = cv2.imread("Steven.jpg")
+img = cv2.imread("dinosaur.png")
 img[img != 255] = img[img != 255] + 1
 print(np.shape(img))
 
@@ -24,7 +27,8 @@ print(np.shape(img))
 height, width = np.shape(img)[:2]
 
 ##Get SLIC Segments
-segments_slic = slic(img, n_segments=NUM_SEGMENTS, sigma=SIGMA, enforce_connectivity=ENFORCE_CONNECT, compactness=COMPACTNESS)
+#segments_slic = slic(img, n_segments=NUM_SEGMENTS, sigma=SIGMA, enforce_connectivity=ENFORCE_CONNECT, compactness=COMPACTNESS)
+segments_slic = segmentation.segment(img, 10,10, 250)
 print(np.max(segments_slic))
 NUM_CREATED = np.max(segments_slic)
 
@@ -32,10 +36,14 @@ NUM_CREATED = np.max(segments_slic)
 segments = np.zeros((NUM_CREATED+1,height,width), dtype=np.uint8)
 for i in range(0, NUM_CREATED+1):
     segments[i][segments_slic == i] = 255
+    #cv2.imshow(str(i), segments[i])
 
-    ##Erode segments slightly
-    segments[i] = cv2.morphologyEx(segments[i], cv2.MORPH_ERODE, kernel= circle_n(2) )
-
+    i2 = str(i) + str(2)
+    ##Morphology things and stuff
+    segments[i] = cv2.morphologyEx(segments[i], cv2.MORPH_CLOSE, kernel = circle_n(2))
+    segments[i] = cv2.morphologyEx(segments[i], cv2.MORPH_OPEN, kernel=circle_n(2))
+    segments[i] = cv2.morphologyEx(segments[i], cv2.MORPH_ERODE, kernel=circle_n(3))
+    #cv2.imshow(i2, segments[i])
 
 ##Set pixel intensities of each segment to the average of the segment
 for i in range(0, NUM_CREATED+1):
@@ -53,32 +61,26 @@ for i in range(0, NUM_CREATED+1):
 finCanvas[finCanvas == 0] = 80
 
 ##Shift some pixel intensities slightly to make the image not perfect
-MAX_RAND = 35 #Max value a pixel intensity can change
-for y in range(0, height):
-    for x in range(0, width):
-        if(random.randint(0, 5) == 5):
-            rand_add = random.randint(5, MAX_RAND)
+#MAX_RAND = 35 #Max value a pixel intensity can change
+#for y in range(0, height):
+#    for x in range(0, width):
+#        if(random.randint(0, 5) == 5):
+#            rand_add = random.randint(5, MAX_RAND)
+#
+#            if(finCanvas[y,x,0] + rand_add < 255):
+#               finCanvas[y,x,0] = finCanvas[y,x,0] + rand_add
+#            else:
+#                finCanvas[y, x, 0] = 255
 
-            if(finCanvas[y,x,0] + rand_add < 255):
-               finCanvas[y,x,0] = finCanvas[y,x,0] + rand_add
-            else:
-                finCanvas[y, x, 0] = 255
+#            if (finCanvas[y, x, 1] + rand_add < 255):
+#                finCanvas[y, x, 1] = finCanvas[y, x, 1] + rand_add
+#            else:
+#                finCanvas[y, x, 1] = 255
 
-            if (finCanvas[y, x, 1] + rand_add < 255):
-                finCanvas[y, x, 1] = finCanvas[y, x, 1] + rand_add
-            else:
-                finCanvas[y, x, 1] = 255
-
-            if (finCanvas[y, x, 2] + rand_add < 255):
-                finCanvas[y, x, 2] = finCanvas[y, x, 2] + rand_add
-            else:
-                finCanvas[y, x, 2] = 255
-
-            #if((finCanvas[y,x,0] + rand_add < 255) & (finCanvas[y,x,1] + rand_add < 255) & (finCanvas[y,x,2] + rand_add < 255)):
-            #  print("yes", finCanvas[y,x])
-            #   finCanvas[y,x] = finCanvas[y,x] + rand_add#random.randint(5,MAX_RAND
-            #else:
-            #    finCanvas[y,x] = 255
+#            if (finCanvas[y, x, 2] + rand_add < 255):
+#                finCanvas[y, x, 2] = finCanvas[y, x, 2] + rand_add
+#            else:
+#                finCanvas[y, x, 2] = 255
 
 
 ##Cut off pixel intensities too large or small
--- segmentation.py
+++ segmentation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-SUPERPIXELS_TO_CHECK = 4
+SUPERPIXELS_TO_CHECK = 10
 
 
 def dist(image, superpixel):
@@ -23,20 +23,44 @@ def segment(img, k_r, k_c, m):
     superpixel_centers_lab = np.zeros((k, 3), dtype=np.int32)
     pixel_list = np.zeros((h, w), dtype=np.int32)
 
+
     for i in range(0, k_r):
         for j in range(0, k_c):
             superpixel_centers_xy[(i * k_c) + j] = [s_r // 2 + s_r * i, s_c // 2 + s_c * j]
             superpixel_centers_lab[(i * k_c) + j] = img[superpixel_centers_xy[(i * k_c) + j][0], superpixel_centers_xy[(i * k_c) + j][1]]
 
-    for x in range(0, w):
-        for y in range(0, h):
-            d_xy = dist(np.array([x, y]), superpixel_centers_xy)
-            d_lab = dist_color(img[x, y], superpixel_centers_lab)
+    print("centers:", np.shape(superpixel_centers_xy))
+
+    print("Initial SLIC")
+    for y in range(0, h):
+        for x in range(0, w):
+            d_xy = dist(np.array([y, x]), superpixel_centers_xy)
+            d_lab = dist_color(img[y, x], superpixel_centers_lab)
             d = d_lab + (m / s * d_xy)
             superpixel_num = np.argmin(d)
-            pixel_list[x, y] = superpixel_num
+            pixel_list[y, x] = superpixel_num
 
-            if y == 0:
-                print(int((x/h)*100), "%")
+
+    for i in range(0, 10):
+      print("Recenter and Slic #", i+1)
+
+      print("RE-CENTERING")
+      for i in range(0, k):
+          row, col = np.where(pixel_list == i)
+          superpixel_centers_xy[i] = [np.average(row), np.average(col)]
+
+      for i in range(0, k_r):
+        for j in range(0, k_c):
+          superpixel_centers_lab[(i * k_c) + j] = img[superpixel_centers_xy[(i * k_c) + j][0], superpixel_centers_xy[(i * k_c) + j][1]]
+
+      print("RE-SLICCING")
+      for y in range(0, h):
+         for x in range(0, w):
+            d_xy = dist(np.array([y, x]), superpixel_centers_xy)
+            d_lab = dist_color(img[y, x], superpixel_centers_lab)
+            d = d_lab + (m / s * d_xy)
+            superpixel_num = np.argmin(d)
+            pixel_list[y, x] = superpixel_num
 
     return pixel_list
+
